d9de92b68c2298538f222ac5bee9ed74bf246c02
mattsun <sjf.matt@gmail.com>
2015-10-27 16:59:32 -0700

    update README.md

---
+++ deliverable/bin/imlcp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+unset CLASSPATH
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
+LIB_HOME=$DIR/lib
+
+for file in "${LIB_HOME}"/*.jar
+do
+  if [ ! -z "$CLASSPATH" ]; then
+    CLASSPATH=${CLASSPATH}":"$file
+  else
+    CLASSPATH=$file
+  fi
+done
+CLASSPATH=$DIR/conf:$CLASSPATH
+java -cp "$CLASSPATH" -DCONTENTPUMP_HOME="$DIR/lib" -Dfile.encoding=UTF-8 $JVM_OPTS -Djava.library.path="$DIR/lib/native" -Djavax.security.auth.useSubjectCredsOnly=false imlcp.imlcp.InteractiveShell $*
